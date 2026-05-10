4506381bd046ce9397622ca23f8a163b4f6b8cc4
yogeshbadgujar524-max <yogeshbadgujar524@gmail.com>
2026-05-10 16:19:22 +0530

    Added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    build: {
+    chunkSizeWarningLimit: 1000,
+  },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
 })
